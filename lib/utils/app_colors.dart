e0b9d121944681fec8210b42197304dc2a7c351d
lagman5006 <asliddin5006@gmail.com>
2025-05-20 19:19:29 +0500

    utils

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color mainColor = Color(0xff1E88E5);
+  static final Color blackColor = Colors.black;
+  static final Color whiteColor = Colors.white;
+  static final Color lightBlue = Color(0xFF90CAF9);
+}
