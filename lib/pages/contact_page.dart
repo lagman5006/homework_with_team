f6c43118694b35410f9f27a8380e2abc37f6aa09
Mamasoliyev <nodirbekmamasoliyev7@gmail.com>
2025-05-20 22:07:12 +0500

    continue

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:homework_with_team/extensions/build_container_ext.dart';
 import 'package:homework_with_team/extensions/build_text_ext.dart';
 import 'package:homework_with_team/extensions/sized_box_ext.dart';
+import 'package:homework_with_team/utils/app_colors.dart';
 import 'package:homework_with_team/utils/app_media.dart';
 
 class ContactPage extends StatelessWidget {
@@ -8,12 +10,62 @@ class ContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
-        title: "Charlotte Diaz".buildText(fontSize: 17, fontWeight: "w600"),
-        actions: [Icon(Icons.edit), 15.w],
+      backgroundColor: AppColors.whiteColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.arrow_back),
+                  25.w,
+                  "Charlotte Diaz".buildText(fontSize: 25, fontWeight: "w600"),
+                  Spacer(),
+                  Icon(Icons.edit),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  65.w,
+                  Image.asset(AppMedia.personGirlPath),
+                  15.w,
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: "Friend"
+                        .buildText(color: AppColors.whiteColor, fontSize: 19)
+                        .buildContainer(
+                          height: 30,
+                          color: AppColors.mainColor,
+                          borderRadius: 17,
+                        ),
+                  ),
+                ],
+              ),
+              20.h,
+              BuildRow(),
+            ],
+          ),
+        ),
       ),
-      body: Column(children: [Image.asset(AppMedia.personGirlPath)]),
+    );
+  }
+}
+
+class BuildRow extends StatelessWidget {
+  const BuildRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.phone),
+        25.w,
+        "123-456-7890".buildText(fontSize: 18),
+      ],
     );
   }
 }
